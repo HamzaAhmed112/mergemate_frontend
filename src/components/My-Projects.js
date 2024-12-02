6589c4db0ed25e153e6ea72a553cf446204c0dbb
HamzaAhmed112 <criticalhitter31@gmail.com>
2024-12-02 08:51:09 +0500

    changed to case sensitive 2

--- src/components/My-Projects.js
+++ src/components/My-Projects.js
@@ -5,7 +5,7 @@ import {Button} from "@/components/ui/button"
 import {ScrollArea} from "@/components/ui/scroll-area"
 import {Separator} from "@/components/ui/separator"
 import {PlusCircle} from 'lucide-react'
-import {AddProjectDialog} from "@/components/add-project-dialog"
+import {AddProjectDialog} from "@/components/Add-Project-Dialog"
 import {ProjectCard} from "@/components/ui components custom/project-card"
 
 export function MyProjects({token}) {
